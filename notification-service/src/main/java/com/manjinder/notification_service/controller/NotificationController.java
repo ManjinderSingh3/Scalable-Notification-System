b0af23edadbfc2b505ad9860de0ad29da9e99df5
Manjinder <msinghw3@gmail.com>
2025-09-10 19:01:18 -0500

    Added database endpoint to Properties file using ENV variables, and added new dependencies in POM file

--- notification-service/src/main/java/com/manjinder/notification_service/controller/NotificationController.java
+++ notification-service/src/main/java/com/manjinder/notification_service/controller/NotificationController.java
@@ -1,13 +1,23 @@
 package com.manjinder.notification_service.controller;
 
 import org.apache.kafka.common.errors.InvalidRequestException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import com.manjinder.notification_service.model.NotificationRequest;
 
 @RestController
-@RequestMapping("/api")
+//@RequestMapping("/api")
 public class NotificationController {
+
+    @Value("${spring.datasource.url}")
+    private String databaseURL;
+
+    @GetMapping("/")
+    public String root(){
+        return databaseURL;
+    }
 
     @GetMapping("/health")
     public String getHealth(){
